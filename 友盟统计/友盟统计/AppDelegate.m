5b85400d05e8e7408ba606ab96e7cc6daca0febf
macbook <592944119@qq.com>
2016-05-16 10:22:08 +0800

    上传

--- 友盟统计/友盟统计/AppDelegate.m
+++ 友盟统计/友盟统计/AppDelegate.m
@@ -17,6 +17,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    UMConfigInstance.appKey = @"xxxxxxxxxxxxxx...";
+    UMConfigInstance.channelId = @"Web";
+    
+    
+    
+    
     return YES;
 }
 
